82bd08cd3275690b561ed9e9719c1e6b55a0d7ef
Hassan Mohammed <hassan-n98@hotmail.com>
2022-01-25 13:54:08 +0300

    done all of the task with the challenge 25/01/2022

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import store from "../store/PetStore";
 
 export default function PetItem({ pet }) {
+  const [petStatus, letPetStatus] = useState(false);
+  const [petImage, setpetImage] = useState(pet.image);
+  const petStatusChange = (change) => {
+    // console.log(pet);
+    letPetStatus(change);
+    if (change === false) {
+      setpetImage(pet.image);
+    } else {
+      setpetImage(pet.image2);
+    }
+  };
   return (
     <div class="col-lg-4 col-md-8 col-sm-10">
       <div class="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={petImage} />
         <div class="content">
           <h3>{pet.name}</h3>
           <button
             type="button"
-            class="btn btn-info"
+            class={`btn m-2 ${!petStatus ? "btn-info" : "btn-danger"}`}
+            onClick={() => petStatusChange(!petStatus)}
+          >
+            {!petStatus ? "Pet" : "Un-pet"}
+          </button>
+          <button
+            type="button"
+            class="btn btn-info m-2"
             onClick={() => store.handleAdopt(pet.id)}
           >
             Adopt
